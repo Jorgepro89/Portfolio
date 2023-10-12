55c6ba7a75f0527fbf9bfe55c3aa85add212a294
AlbertoMorales24 <0237032@up.edu.mx>
2023-10-11 18:53:05 -0600

    Loading page

---
+++ src/loading/loading.js
@@ -0,0 +1,17 @@
+import './loading.css';
+
+function Loading() {
+    return (
+      <div className="loading">
+        <header className="Loading-header">
+          
+          <div className="loading-animated">
+            We are <span></span>
+          </div>
+         
+        </header>
+      </div>
+    );
+  }
+
+  export default Loading;
